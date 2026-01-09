5419f51410df5d3cc7de11de9d300c4a7cdc8ab6
Fayash Wahyudha <wahyudhafayash@fayass-MaacBook.local>
2026-01-09 23:58:51 +0700

    feat(ProductDetail): tambah page Product Detail dan component Product Action

---
+++ app/(landing)/_components/product-detail/ProductAction.tsx
@@ -0,0 +1,51 @@
+"use client";
+import { useEffect, useState } from "react";
+import Button from "../ui/Button";
+import {
+  FiArrowRight,
+  FiChevronDown,
+  FiChevronUp,
+  FiShoppingBag,
+} from "react-icons/fi";
+
+const ProductAction = () => {
+  const [qty, setQty] = useState(1);
+
+  const incrementQty = () => setQty((prevQty) => prevQty + 1);
+  const decrementQty = () =>
+    setQty((prevQty) => (prevQty > 1 ? prevQty - 1 : prevQty));
+
+  return (
+    <div className="flex gap-5">
+      <div className="border border-gray-300 inline-flex w-fit min-w-20.5">
+        <div className="aspect-square text-xl font-medium border-r border-gray-300 flex justify-center items-center">
+          <span>{qty}</span>
+        </div>
+        <div className="flex flex-col">
+          <button
+            className="border-b border-gray-100 cursor-pointer h-1/2 aspect-square flex justify-center items-center"
+            onClick={incrementQty}
+          >
+            <FiChevronUp />
+          </button>
+          <button
+            className="cursor-pointer h-1/2 aspect-square flex justify-center items-center"
+            onClick={decrementQty}
+          >
+            <FiChevronDown />
+          </button>
+        </div>
+      </div>
+      <Button className="px-20 w-full">
+        <FiShoppingBag size={24} />
+        Add to Cart
+      </Button>
+      <Button variant="dark" className="px-20 w-full">
+        Checkout Now
+        <FiArrowRight size={24} />
+      </Button>
+    </div>
+  );
+};
+
+export default ProductAction;
