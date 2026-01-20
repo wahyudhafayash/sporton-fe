38c30a3fae579cf1c5b88627d100afe545b34ed8
fayass <wahyudhafayash@gmail.com>
2026-01-20 17:22:42 +0700

    feat(products): integrasi API backend product dan service API

--- app/types/index.ts
+++ app/types/index.ts
@@ -6,3 +6,15 @@ export interface Category {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface Product {
+  _id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  category: Category;
+  stock: number;
+  price: number;
+  createdAt: string;
+  updatedAt: string;
+}
