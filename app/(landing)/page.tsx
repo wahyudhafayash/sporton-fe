4053affe34aaa6531a63aac5778997ac10ca29e7
fayass <wahyudhafayash@gmail.com>
2026-01-20 22:21:32 +0700

    fix: resolve conflicts and stage changes

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,15 +2,19 @@ import CategoriesSection from "./_components/home/CategoriesSection";
 import HeroSection from "./_components/home/HeroSection";
 import ProductSection from "./_components/home/ProductSection";
 import { getAllCategories } from "../service/category.service";
+import { getAllProducts } from "../service/product.service";
 
 const Home = async () => {
-  const categories = await getAllCategories();
+  const [categories, product] = await Promise.all([
+    getAllCategories(),
+    getAllProducts(),
+  ]);
 
   return (
     <main>
       <HeroSection />
       <CategoriesSection categories={categories} />
-      <ProductSection />
+      <ProductSection products={product} />
     </main>
   );
 };
